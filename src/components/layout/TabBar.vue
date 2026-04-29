f15da51429de0f0d835faba38496451cac6b7bfa
cheney <516396859@qq.com>
2026-04-30 02:16:29 +0800

    不关闭 friday 页面

--- src/components/layout/TabBar.vue
+++ src/components/layout/TabBar.vue
@@ -27,7 +27,7 @@
               class="tab-icon"
             />
             <span class="tab-title">{{ t(tab.i18nKey) }}</span>
-            <button v-show="hoveredTabId === tab.id" class="tab-close-btn" @click.stop="closeTab(tab.id)">
+            <button v-show="hoveredTabId === tab.id && !(tabStore.openedTabs.length === 1 && tab.path === '/friday')" class="tab-close-btn" @click.stop="closeTab(tab.id)">
               <X :size="12" :stroke-width="2" />
             </button>
           </div>
